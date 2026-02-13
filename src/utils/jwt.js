59c67f66f52622e07b75325cdfe1266d3cdb4d51
demo <mometsaleh@gmail.com>
2026-02-13 19:21:32 +0100

    initial backend commit

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 
-const secret = process.env.JWT_ACCESS_SECRET;
-if (!secret) throw new Error("Missing JWT_ACCESS_SECRET");
+const SECRET = process.env.JWT_ACCESS_SECRET;
+if (!SECRET) throw new Error("JWT_ACCESS_SECRET is missing");
 
-const ttl = process.env.JWT_ACCESS_TTL || "15m";
+const TTL = process.env.JWT_ACCESS_TTL || "15m";
 
 function signAccessToken(payload) {
-  return jwt.sign(payload, secret, { expiresIn: ttl });
+  return jwt.sign(payload, SECRET, { algorithm: "HS256", expiresIn: TTL });
 }
 
 function verifyAccessToken(token) {
-  return jwt.verify(token, secret);
+  return jwt.verify(token, SECRET, { algorithms: ["HS256"] });
 }
 
 module.exports = { signAccessToken, verifyAccessToken };
